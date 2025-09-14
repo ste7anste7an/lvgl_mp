54feb0bbd94ecdd0f23e4578ed7b67edf5c0bec6
ste7anste7an <stefan@vmi1504892.contaboserver.net>
2025-09-14 21:05:09 +0200

    changed mpy-robot-tool to main branch

---
+++ cleanup.sh
@@ -0,0 +1,4 @@
+rm -rf  lvgl_micropython
+rm -rf micropython-ulab
+rm -rf submodules
+rm -rf lms-frozen
--- lms-frozen/uartremote.py
+++
@@ -1,9 +0,0 @@
-# Backward compatibility with UartRemote scripts
-try:
-    from .serialtalk.auto import SerialTalk
-except ImportError:
-    from serialtalk.auto import SerialTalk
-
-class UartRemote(SerialTalk):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
