ae01dd3c2eb4291d3bd4ab6ddd532a3bfed994d7
ste7anste7an <ste7an@gmail.com>
2026-01-21 08:16:56 +0000

    Ignore generated subdirectories

---
+++ esp32_components.cmake
@@ -0,0 +1,4 @@
+
+
+include(/home/stefan/esp32/lvgl_mp/mymod_chipinfo/micropython.cmake)
+include(/home/stefan/esp32/lvgl_mp/micropython-ulab/code/micropython.cmake)
--- scripts/get_lms_esp32_modules.sh
+++ scripts/get_lms_esp32_modules.sh
@@ -12,8 +12,8 @@ git clone https://github.com/lvgl-micropython/lvgl_micropython.git
 git clone https://github.com/v923z/micropython-ulab.git
 
 # copy patched esp32_rmt.c
-mkdir -p lvgl_micropython/micropy_updates/esp32
-cp lvgl_patches/micropy_updates/esp32/esp32_rmt.c lvgl_micropython/micropy_updates/esp32
+#mkdir -p lvgl_micropython/micropy_updates/esp32
+#cp lvgl_patches/micropy_updates/esp32/esp32_rmt.c lvgl_micropython/micropy_updates/esp32
 
 cd scripts
 
@@ -22,7 +22,9 @@ cp inisetup.py ../lvgl_micropython/micropy_updates/esp32/modules
 
 cd ../submodules
 git clone https://github.com/v923z/micropython-ulab.git
-git clone https://github.com/antonvh/mpy-robot-tools.git
+git clone https://github.com/antonvh/btbricks.git
+git clone https://github.com/antonvh/np_animation
+git clone https://github.com/antonvh/rcservo
 git clone https://github.com/antonvh/SerialTalk.git
 git clone https://github.com/antonvh/PUPRemote.git
 git clone https://github.com/antonvh/PyHuskyLens.git
@@ -34,11 +36,10 @@ git clone https://github.com/antonvh/PyHuskyLens.git
 cp ../lms_modules/*.py $LMS_ESP32_MODULES
 cp ../submodules/PUPRemote/src/lpf2.py $LMS_ESP32_MODULES
 cp ../submodules/PUPRemote/src/pupremote.py $LMS_ESP32_MODULES
-cp ../submodules/mpy-robot-tools/mpy_robot_tools/servo.py $LMS_ESP32_MODULES
-cp ../submodules/mpy-robot-tools/mpy_robot_tools/np_animation.py $LMS_ESP32_MODULES
-cp ../submodules/mpy-robot-tools/mpy_robot_tools/bt.py $LMS_ESP32_MODULES
-cp ../submodules/mpy-robot-tools/mpy_robot_tools/ctrl_plus.py $LMS_ESP32_MODULES
-cp ../submodules/mpy-robot-tools/mpy_robot_tools/ctrl_plus.py $LMS_ESP32_MODULES
+cp ../submodules/rcservo/servo.py $LMS_ESP32_MODULES
+cp ../submodules/np_animation/np_animation/np_animation.py $LMS_ESP32_MODULES
+mkdir -p $LMS_ESP32_MODULES/btbricks
+cp ../submodules/btbricks/btbricks/*.py $LMS_ESP32_MODULES/btbricks
 cp ../submodules/PyHuskyLens/Library/pyhuskylens.py $LMS_ESP32_MODULES
 cp ../submodules/SerialTalk/uartremote.py $LMS_ESP32_MODULES
 mkdir -p $LMS_ESP32_MODULES/serialtalk
